66c0a999f363260efde9b39b8acf9df1ae155460
Adolfo Kentaro <125566679+Adolfohada@users.noreply.github.com>
2024-06-21 16:06:18 -0300

    Add files via upload

---
+++ basic/print-info.js
@@ -0,0 +1,7 @@
+const cliente = {
+    nome: "Zyzz",
+    idade: 36,
+    cpf:"343414141341",
+    email: "andre@email.com"
+}
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos. `)
